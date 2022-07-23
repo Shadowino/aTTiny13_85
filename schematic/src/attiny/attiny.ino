353a761deb0970c996075248a9d7f94cf627bfc1
Shadowino <savlovichdenis@gmail.com>
2022-07-23 18:18:10 +0300

    скорректированы цвета на схеме, добавлен тестовый код

---
+++ schematic/src/attiny/attiny.ino
@@ -0,0 +1,32 @@
+/*
+  test code for:
+  ATTINY 13\13A\85
+
+  pinout attiny -> Arduino
+  PB1(RST)   -> D10
+  PB2
+  PB3
+  GND        -> GND
+  PB5(MOSI)  -> D11
+  PB6(MISO)  -> D12
+  PB7(SCK)   -> D13
+  VCC        -> 5V
+
+  use programmer "Arduino as ISP"
+  load with programmeк (Ctrl+Shift+U)
+  gooв luck! ^)
+*/
+
+// delay
+#define DEL 100
+
+void setup() {
+  pinMode(PB4, OUTPUT); // PB4 - port with led
+}
+
+void loop() {
+  digitalWrite(PB4, 1);
+  delay(DEL);
+  digitalWrite(PB4, 0);
+  delay(DEL);
+}
